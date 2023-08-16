301e79b2c50761d06e90cfa11d3ca94c3360d695
DilanSenanayake <senanayakadws@gmail.com>
2023-08-16 07:37:32 +0530

    Code

---
+++ Spring Boot/src/main/java/com/example/Test/DependencyInjection.java
@@ -0,0 +1,35 @@
+package com.example.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class DependencyInjection {
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInjection.class, args);
+    }
+}
+
+@Component
+class Subject {
+    private Teacher teacher;
+    // constructor
+    @Autowired
+    Subject(Teacher teacher) {
+        this.teacher = teacher;
+        System.out.println("A instance of subject is creating");
+        teacher.hello();
+    }
+    public void say() {
+        teacher.hello();
+    }
+}
+
+@Component
+class Teacher {
+    public void hello() {
+        System.out.println("Hello");
+    }
+}
